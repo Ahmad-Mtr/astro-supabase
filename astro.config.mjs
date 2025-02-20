9c9ee19922fb7e430de4f1eee5f815fcb64e322a
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2025-02-20 14:24:45 +0000

    update: change astro config to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-supabase-auth.vercel.app",
+  // site: "https://astro-supabase-auth.netlify.app",
   output: "server",
-  adapter: vercel(),
+  // adapter: vercel(),
   integrations: [tailwind(), solidJs()],
+  adapter: netlify({
+    edgeMiddleware: true
+  })
 });
