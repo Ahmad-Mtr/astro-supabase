e47305c875cfdc2e79e025a7770bd0c87f20c2f4
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2025-02-20 14:26:24 +0000

    update: change astro config to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://astro-supabase-auth.netlify.app",
+  site: "https://astro-supabase-auth.netlify.app",
   output: "server",
   // adapter: vercel(),
   integrations: [tailwind(), solidJs()],
